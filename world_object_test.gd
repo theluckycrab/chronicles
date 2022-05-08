367ffc4549d50478d26132f2a2d50fcf2ddad5b2
FK <tluckycrab@gmail.com>
2022-05-07 21:09:57 -0500

    Rigidbody world items prototyped

---
+++ world_object_test.gd
@@ -0,0 +1,15 @@
+extends Spatial
+
+var item = Item.new()
+
+onready var mesh = $KinematicBody/MeshInstance
+onready var body = $KinematicBody
+onready var shape = $KinematicBody/CollisionShape.get_shape()
+
+
+func _ready():
+	mesh.mesh = load(item.stats.mesh_file_path)
+	var mpos = mesh.mesh.get_aabb()
+	mpos = mpos.position + (mpos.size / 2)
+	$KinematicBody/CollisionShape.transform.origin = mpos
+	shape.extents = mesh.mesh.get_aabb().size / 2
