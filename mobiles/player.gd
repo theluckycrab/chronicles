40fbc1994e9531b229469871938c2de72937ee91
FK <tluckycrab@gmail.com>
2022-07-20 19:27:06 -0500

    interpolation tutorial, literally just copying game development center's youtube videos

--- mobiles/player.gd
+++ mobiles/player.gd
@@ -6,7 +6,7 @@ func _physics_process(delta):
 	var wasd:Vector3 = Vector3.ZERO
 	wasd.x = (Input.get_action_strength("a") - Input.get_action_strength("d"))
 	wasd.z = (Input.get_action_strength("w") - Input.get_action_strength("s"))
-	move_and_slide(wasd + Vector3(0,-9,0), Vector3.UP)
+	move_and_slide(wasd * 5 + Vector3(0,-9,0), Vector3.UP)
 	define_player_state()
 
 func define_player_state():
--- scenes/lobi_town/lobi_town.gd
+++ scenes/lobi_town/lobi_town.gd
@@ -1,6 +1,27 @@
 extends Spatial
 
 var last_world_state = 0
+var world_state_buffer = []
+const interpolation_offset = 100
+
+func _physics_process(delta):
+	var render_time = OS.get_system_time_msecs() - interpolation_offset
+	if world_state_buffer.size() > 1:
+		while world_state_buffer.size() > 2 and render_time > world_state_buffer[1].T:
+			world_state_buffer.remove(0)
+		var interpolation_factor = float(render_time - world_state_buffer[0]["T"]) / float(world_state_buffer[1]["T"] - world_state_buffer[0]["T"])
+		for player in world_state_buffer[1].keys():
+			if str(player) == "T":
+				continue
+			if player == get_tree().get_network_unique_id():
+				continue
+			if ! world_state_buffer[0].has(player):
+				continue
+			if get_node("OtherPlayers").has_node(str(player)):
+				var new_position = lerp(world_state_buffer[0][player]["P"], world_state_buffer[1][player]["P"], interpolation_factor)
+				get_node("OtherPlayers/"+str(player)).global_transform.origin = new_position
+			else:
+				spawn_new_player(player, world_state_buffer[1][player]["P"])
 
 func spawn_new_player(who, where):
 	if get_tree().get_network_unique_id() == who:
@@ -22,11 +43,4 @@ func despawn_player(who):
 func update_world_state(world_state):
 	if world_state["T"] > last_world_state:
 		last_world_state = world_state["T"]
-		world_state.erase("T")
-		world_state.erase(get_tree().get_network_unique_id())
-		for player in world_state.keys():
-			var p = get_node_or_null("OtherPlayers/"+str(player))
-			if p != null:
-				p.global_transform.origin = world_state[player]["P"]
-			else:
-				spawn_new_player(player, world_state[player]["P"])
+		world_state_buffer.append(world_state)
