35dedecd6af430b313057f38213ba865b6acd674
FK <tluckycrab@gmail.com>
2022-12-24 23:36:07 -0600

    camera now has vertical control, limits, and inversion. page up/down to invert v/h

--- player_camera.gd
+++ player_camera.gd
@@ -2,8 +2,16 @@ extends Spatial
 
 var last_mouse_relative = Vector2.ZERO
 var h_sens = 0.25
+var v_sens = 0.25
+var v_invert = -1
+var h_invert = 1
+var min_v_angle = deg2rad(-25)
+var max_v_angle = deg2rad(25)
 var views = []
 var stored_delta = 0
+
+onready var v = $Vertical
+onready var camera = $Vertical/Camera
 
 func _ready():
 	build_views()
@@ -13,21 +21,26 @@ func _ready():
 func _input(event):
 	if event is InputEventMouseMotion:
 		last_mouse_relative = event.relative
-		rotation.y = lerp_angle(rotation.y, rotation.y - deg2rad(last_mouse_relative.x), h_sens)
+		apply_rotation()
+		apply_limits()
+
 	if event.is_action_released("ui_cancel"):
 		if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 		else:
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+			
+	if event.is_action_released("ui_page_up"):
+		v_invert *= -1
+	if event.is_action_released("ui_page_down"):
+		h_invert *= -1
+			
 	if event.as_text() as int < views.size():
 		var v = event.as_text() as int
 		if v == 0:
 			if event.as_text() != "0":
 				return
 		change_view(v)
-		
-func _process(delta):
-	stored_delta = delta
 		
 func build_views():
 	for i in get_children():
@@ -37,6 +50,13 @@ func build_views():
 		
 func change_view(v):
 	print("Switching to Camera ", v)
-	$Camera.global_transform = views[v].global_transform
-	$Camera.rotation.y = deg2rad(-180)
+	camera.global_transform = views[v].global_transform
+	camera.rotation.y = deg2rad(-180)
 	rotation.y = views[v].rotation.y
+
+func apply_rotation():
+	rotation.y = lerp_angle(rotation.y, rotation.y - deg2rad(last_mouse_relative.x), h_sens * h_invert)
+	v.rotation.x = lerp_angle(v.rotation.x, v.rotation.x - deg2rad(last_mouse_relative.y), v_sens * v_invert)
+	
+func apply_limits():
+	v.rotation.x = clamp(v.rotation.x, min_v_angle, max_v_angle)
