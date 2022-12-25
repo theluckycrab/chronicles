f971e576fc3429138a6e160418b37fd5dc36936d
FK <tluckycrab@gmail.com>
2022-12-24 23:22:32 -0600

    moved camera processing into _input to avoid clipping

--- player_camera.gd
+++ player_camera.gd
@@ -1,7 +1,9 @@
 extends Spatial
 
 var last_mouse_relative = Vector2.ZERO
+var h_sens = 0.25
 var views = []
+var stored_delta = 0
 
 func _ready():
 	build_views()
@@ -11,6 +13,7 @@ func _ready():
 func _input(event):
 	if event is InputEventMouseMotion:
 		last_mouse_relative = event.relative
+		rotation.y = lerp_angle(rotation.y, rotation.y - deg2rad(last_mouse_relative.x), h_sens)
 	if event.is_action_released("ui_cancel"):
 		if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -23,12 +26,8 @@ func _input(event):
 				return
 		change_view(v)
 		
-		
-		
 func _process(delta):
-	if last_mouse_relative != Vector2.ZERO:
-		rotation.y = lerp_angle(rotation.y, rotation.y - deg2rad(last_mouse_relative.x), 26 * delta)
-		last_mouse_relative = Vector2.ZERO
+	stored_delta = delta
 		
 func build_views():
 	for i in get_children():
