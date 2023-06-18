2f97a795d4788665e8696fd944b9d0485b3f4108
FK <tluckycrab@gmail.com>
2023-06-18 06:17:29 -0500

    added delete and new character to character_selection

--- scenes/character_select.gd
+++ scenes/character_select.gd
@@ -10,24 +10,33 @@ onready var back_button = $Control/HBoxContainer2/BackButton
 onready var name_list = $Control/HBoxContainer/NameList
 onready var item_list = $Control/HBoxContainer3/ItemList
 onready var preview_model = $Armature
-onready var current_char_name_label = $Control/CurrentCharName/Label
+onready var name_edit_button = $Control/NameEditButton
+onready var delete_button = $Control/VBoxContainer/DeleteButton
+onready var new_button = $Control/VBoxContainer/NewCharacterButton
+onready var name_entry_form = $Control/NameEntryForm
+onready var name_entry = $Control/NameEntryForm/NameEntry
 
 func _ready():
 	generate_nameplates()
 	on_char_button(Data.get_config_value("last_character"))
 	join_button.connect("button_down", self, "on_join")
 	back_button.connect("button_down", self, "on_back")
+	delete_button.connect("button_down", self, "on_delete")
+	new_button.connect("button_down", self, "on_new")
+	name_edit_button.connect("button_down", self, "on_name_edit")
+	name_entry.connect("text_entered", self, "on_name_entry")
 
 func generate_nameplates():
 	for i in name_list.get_children():
 		i.queue_free()
+	characters.clear()
 	var dir = Directory.new()
 	var file = File.new()
 	dir.open("user://saves/")
 	dir.list_dir_begin()
 	var file_name = dir.get_next()
 	while file_name != "":
-		if file_name != "." and file_name != "..":
+		if file_name != "." and file_name != ".." and file_name != "new_character" and file_name != "null_edgelord,_grand_orator_of_testers":
 			characters.append(file_name)
 		file_name = dir.get_next()
 	dir.list_dir_end()
@@ -42,7 +51,10 @@ func on_char_button(n):
 	for i in item_list.get_children():
 		i.queue_free()
 	Data.load_char_data(n)
-	char_data = Data.get_char_data()
+	generate_item_list(Data.get_char_data())
+	
+	
+func generate_item_list(char_data):
 	for i in char_data.equipment:
 		var item = Data.get_item(i)
 		preview_model.equip(item)
@@ -50,8 +62,8 @@ func on_char_button(n):
 			var l = item_label.instance()
 			l.set_text(Data.get_item(i).current.name)
 			item_list.add_child(l)
-	current_char_name_label.set_text(char_data.name)
-	current_char_name_label.add_color_override("font_color", char_data.chat_color)
+	name_edit_button.set_text(char_data.name)
+	name_edit_button.label.add_color_override("font_color", char_data.chat_color)
 		
 func on_join() -> void:
 	Client.join()
@@ -60,3 +72,27 @@ func on_join() -> void:
 	
 func on_back():
 	Events.emit_signal("scene_change_request", "main_menu")
+
+func on_delete():
+	var dir = Directory.new()
+	dir.remove("user://saves/"+Data.get_snake_case(Data.get_char_data().name))
+	if characters.empty():
+		on_char_button("new_character")
+	else:
+		on_char_button(characters.front())
+	generate_nameplates()
+	
+func on_new():
+	on_char_button("new_character")
+
+func on_name_edit():
+	name_entry_form.show()
+	
+func on_name_entry(n):
+	print("yo")
+	var dir = Directory.new()
+	name_entry_form.hide()
+	name_edit_button.set_text(n)
+	Data.set_char_value("name", n)
+	Data.save_char()
+	generate_nameplates()
--- ui/labeled_button.gd
+++ ui/labeled_button.gd
@@ -1,4 +1,6 @@
 extends TextureButton
 
+onready var label = $Label
+
 func set_text(t: String) -> void:
 	$Label.text = t
